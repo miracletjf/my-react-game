1e8a7e9064667b4c29c95ca5577c7e655fbb6f40
miracletjf <1398988546@qq.com>
2021-03-04 21:28:34 +0800

    计算获胜者

--- src/index.js
+++ src/index.js
@@ -15,11 +15,12 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true
+      xIsNext: true,
     }
   }
   handleClick (i) {
     const squares = this.state.squares.slice()
+    if(calculateWinner(squares) || squares[i]) return false;
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares, 
@@ -36,7 +37,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner is ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
@@ -75,6 +82,24 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for(const line of lines) {
+    const [a, b, c] = line
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) return squares[a]
+  }
+  return null;
 }
 
 // ========================================
