f7cfe8fd0dd59adaa44adeecc3c21795bdc86ca2
miracletjf <1398988546@qq.com>
2021-03-05 00:20:03 +0800

    优化棋盘 生成方式 使用两个循环来渲染出棋盘的格子

--- src/index.js
+++ src/index.js
@@ -21,25 +21,17 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    const rows = Array.from({length: 3}, (row, rowIndex) => {
+      const cols = Array.from({length: 3}, (col, colIndex) => {
+        return this.renderSquare(rowIndex * 3 + colIndex)
+      })
+      return (
+        <div className="board-row" key={rowIndex}>
+          {cols}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      )
+    })
+    return <div> {rows} </div>;
   }
 }
 
