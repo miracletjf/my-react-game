4ff853ad58700aefb0886b025c35c9206699f361
miracletjf <1398988546@qq.com>
2021-03-04 23:14:04 +0800

    完善 时光穿梭 功能（游戏存档）

--- src/index.js
+++ src/index.js
@@ -50,24 +50,44 @@ class Game extends React.Component {
       history: [
         { squares: Array(9).fill(null) }
       ],
+      stepNumber: 0,
       xIsNext: true
     }
   }
   handleClick (i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if(calculateWinner(squares) || squares[i]) return false;
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat([{squares}]),
-      xIsNext: !this.state.xIsNext
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+  jumpTo (stepNumber) {
+    this.setState({
+      stepNumber,
+      xIsNext: stepNumber % 2 === 0
     })
   }
   render() {
     const history = this.state.history
-    const current = history[history.length-1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        `Go to move # ${move}` :
+        `Go to start`
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = `Winner is ${winner}`
@@ -84,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
